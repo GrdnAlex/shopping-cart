183ac188bc394fe19d4c43336d0b7d2caa544b7c
GrdnAlex <ag5443@stern.nyu.edu>
2022-08-04 00:30:49 -0400

    Formatted Version with All Requirements

--- shopping_cart.py
+++ shopping_cart.py
@@ -58,13 +58,16 @@ while True:
 
 print("----------------------------")
 print("Campus Convenience Shop")
+print("1 Peach St. New York, NY 10019")
+print("(917) 804-6237")
 print("----------------------------")
+
 # importing required modules
 import datetime
 
 curr_datetime = datetime.datetime.now()
 
-print("CHECKOUT AT:" + str(curr_datetime))
+print("CHECKOUT AT:" + str(curr_datetime.strftime("%c")))
 print("----------------------------")
 
 print("Selected Products:")
@@ -97,8 +100,7 @@ print("----------------------------")
 # The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
 # The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
 # The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)## The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
 # A friendly message thanking the customer and/or encouraging the customer to shop again
 
 
